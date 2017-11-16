2847607a89452c66f29045cef8c83b7fac10cbb3
Ram Moore <rammoore112358@gmail.com>
2017-11-16 12:54:44 -0600

    changing lab 12

--- lab012h.cpp
+++ lab012h.cpp
@@ -6,18 +6,15 @@ int fibonacci(int n);
 int factorial(int n);
 
 int main(){
-    int menu, n;
-    cout << "Enter 1 for ! and 2 for a fibonacci boy: ";
-    cin >> menu;
-    cout << "Enter the number you want to compute the factorial of or thenth digit of fibonacci: ";
+    int n;
+    cout << "Enter the number: ";
     cin >> n;
-    menu == 1 ? cout << factorial(n) : cout << fibonacci(n);
-
+    cout << "Factorial of "<< n << ": "<< factorial(n);
+    cout << n <<"th fibonaci number: " << fibonacci(n);
     return 0;
 }
 
 int factorial(int n){
-    //cout << n;
     return n == 1 ? n : n * factorial(n - 1);
 }
 
